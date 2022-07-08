2b99e59c3ea6da20b77db8ebee1e47208a999c9e
Wiktor Maciązek <75098062+swapperDEV@users.noreply.github.com>
2022-07-08 17:59:56 +0200

    1.0

--- components/Views/CenterViews/HashtagsView/HashtagsSearch.tsx
+++ components/Views/CenterViews/HashtagsView/HashtagsSearch.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import hashtagsStyles from './hashtagssearch.module.scss'
-import Posts from '../../../Posts/PostsMapper';
-import { useRouter } from 'next/router';
+import React from "react";
+import hashtagsStyles from "./hashtagssearch.module.scss";
+import Posts from "../../../Posts/PostsMapper";
+import { useRouter } from "next/router";
 export const HashtagsSearch = () => {
-    const path = useRouter()
-    return (
-        <>
-        <div className={hashtagsStyles.wrapper}>
-            <div className={hashtagsStyles.content}>
-                <Posts requirements={path.query.hashtag} requirementsType="hashtag"/>
-            </div>
+  const path = useRouter();
+  console.log(typeof path.query.hashtag, "zwraca");
+  return (
+    <>
+      <div className={hashtagsStyles.wrapper}>
+        <div className={hashtagsStyles.content}>
+          {typeof path.query.hashtag === "string" ? (
+            <>
+              {" "}
+              <Posts
+                requirements={path.query.hashtag}
+                requirementsType="hashtag"
+              />
+            </>
+          ) : null}
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
